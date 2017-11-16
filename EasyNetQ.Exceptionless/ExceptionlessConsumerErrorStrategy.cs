c4e1f7d53e70f7ef849276854b8c021c6d89d60b
Matt Davey <matt.davey@fsfe.org>
2017-11-16 20:33:56 +0000

    Add Exceptionless consumer error strategy

---
+++ EasyNetQ.Exceptionless/ExceptionlessConsumerErrorStrategy.cs
@@ -0,0 +1,38 @@
+﻿namespace EasyNetQ.Exceptionless
+{
+    using System;
+    using EasyNetQ;
+    using EasyNetQ.Consumer;
+
+    using global::Exceptionless;
+
+    class ExceptionlessConsumerErrorStrategy : DefaultConsumerErrorStrategy {
+        readonly ExceptionlessClient _client;
+        readonly Action<EventBuilder, ConsumerExecutionContext> _intercept;
+
+        public ExceptionlessConsumerErrorStrategy(IConnectionFactory connectionFactory, ISerializer serializer, IEasyNetQLogger logger, IConventions conventions, ITypeNameSerializer typeNameSerializer, IErrorMessageSerializer errorMessageSerializer, ExceptionlessClient client = null, Action<EventBuilder, ConsumerExecutionContext> intercept = null)
+            : base(connectionFactory, serializer, logger, conventions, typeNameSerializer, errorMessageSerializer) {
+            _client = client ?? ExceptionlessClient.Default;
+            _intercept = intercept;
+        }
+
+        public override AckStrategy HandleConsumerError(ConsumerExecutionContext context, Exception exception) {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (exception == null)
+                throw new ArgumentNullException(nameof(exception));
+
+            try {
+                return base.HandleConsumerError(context, exception);
+            }
+            finally {
+                var eventBuilder = exception.ToExceptionless();
+
+                _intercept?.Invoke(eventBuilder, context);
+
+                eventBuilder.Submit();
+            }
+        }
+    }
+}
---
+++ EasyNetQ.Exceptionless/ServiceRegisterExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace EasyNetQ.Exceptionless
+{
+    using System;
+    using EasyNetQ;
+    using EasyNetQ.Consumer;
+
+    using global::Exceptionless;
+
+    /// <summary>
+    /// Extension methods for the <see cref="IServiceRegister"/> interface.
+    /// </summary>
+    public static class ServiceRegisterExtensions {
+        /// <summary>
+        /// Enable reporting message consumer errors to Exceptionless.
+        /// </summary>
+        /// <param name="serviceRegister">Extension instance.</param>
+        /// <param name="client">Optional Exceptionless client. Defaults to ExceptionlessClient.Default.</param>
+        /// <param name="intercept">Optional interceptor hook, allowing you to customize the exceptionless event.</param>
+        /// <returns>Extension instance.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if the value passed to the <paramref name="serviceRegister"/> parameter is <c>null</c>.
+        /// </exception>
+        public static IServiceRegister EnableExceptionless(this IServiceRegister serviceRegister, ExceptionlessClient client = null, Action<EventBuilder, ConsumerExecutionContext> intercept = null) {
+            if (serviceRegister == null)
+                throw new ArgumentNullException(nameof(serviceRegister));
+
+            serviceRegister.Register<IConsumerErrorStrategy>(services => new ExceptionlessConsumerErrorStrategy(
+                services.Resolve<IConnectionFactory>(),
+                services.Resolve<ISerializer>(),
+                services.Resolve<IEasyNetQLogger>(),
+                services.Resolve<IConventions>(),
+                services.Resolve<ITypeNameSerializer>(),
+                services.Resolve<IErrorMessageSerializer>(),
+                client,
+                intercept
+            ));
+
+            return serviceRegister;
+        }
+    }
+}
